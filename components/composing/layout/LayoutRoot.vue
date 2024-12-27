b8cd714be8057742518058e51146ac786c71440a
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-27 15:31:30 +0100

    :rocket: Setup composing components

---
+++ components/composing/layout/LayoutRoot.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+
+interface LayoutProps {
+  name?: string;
+  class?: HTMLAttributes["class"];
+}
+const props = defineProps<LayoutProps>();
+</script>
+
+<template>
+  <div :data-layout="props.name ?? 'default'">
+    <slot />
+  </div>
+</template>
---
+++ components/composing/page/PageRoot.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+
+interface PageProps {
+  name?: string;
+  class?: HTMLAttributes["class"];
+}
+const props = defineProps<PageProps>();
+</script>
+
+<template>
+  <div :data-page="props.name ?? 'home'">
+    <slot />
+  </div>
+</template>
