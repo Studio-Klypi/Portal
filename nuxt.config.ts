143e5cb9a5584aebef670b4fa7e8d4220d9cd872
loicmaes <97891718+loicmaes@users.noreply.github.com>
2024-12-28 14:20:20 +0100

    :speech_balloon: Add locales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,7 +26,7 @@ export default defineNuxtConfig({
   i18n: {
     vueI18n: "./i18n.config.ts",
     defaultLocale: "fr",
-    locales: ["fr", "en"],
+    locales: ["fr", "en", "de"],
   },
   tailwindcss: {
     cssPath: ["assets/css/tailwind.css", { injectPosition: "first" }],
