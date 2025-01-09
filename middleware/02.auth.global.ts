7658b4eacecb6e5008ed4e6700ad52a4c97adf86
loicmaes <production@maesloic.fr>
2025-01-09 18:30:31 +0100

    :rocket: Add auth frontend middleware

---
+++ middleware/02.auth.global.ts
@@ -0,0 +1,15 @@
+export default defineNuxtRouteMiddleware(async (to, from) => {
+  const user = useUser();
+
+  if (!user.value) {
+    if (to.path.includes("/auth/login"))
+      return;
+    return navigateTo("/auth/login");
+  }
+
+  if (to.path.includes("/auth/login")) {
+    if (from)
+      return navigateTo("/");
+    return abortNavigation();
+  }
+})
