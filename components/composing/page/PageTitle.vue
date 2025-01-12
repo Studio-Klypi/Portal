52e03a28bb5a0f5f9371adfe383d0f0bb984bf15
loicmaes <production@maesloic.fr>
2025-01-12 18:38:27 +0100

    :rocket: Add page composing components

---
+++ components/composing/page/PageTitle.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from "vue";
+import { cn } from "~/lib/utils";
+
+const props = defineProps<{
+  class?: HTMLAttributes["class"];
+}>();
+</script>
+
+<template>
+  <h1 :class="cn('text-2xl font-extrabold', props.class)">
+    <slot />
+  </h1>
+</template>
